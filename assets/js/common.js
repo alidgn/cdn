e55e85373ec89da931b92f6954fc625f9d46ac67
Ali Doğan <alidogan915@gmail.com>
2023-06-14 18:18:39 +0300

    .

--- assets/js/common.js
+++ assets/js/common.js
@@ -7,7 +7,21 @@
 
     const element = document.getElementById("footer-email");
     if (element) {
-        const html = `<a href="mailto:${email.address}">${email.display}</a>`;
+        const html = `<a href="mailto:${email.address}" class="email">${email.display}</a>`;
         element.innerHTML = html;
+    }
+
+    var css = 'a.email { text-decoration: none !important; } a#footer-email { text-decoration: none !important; }',
+        head = document.head || document.getElementsByTagName('head')[0],
+        style = document.createElement('style');
+
+    head.appendChild(style);
+    style.type = 'text/css';
+
+    if (style.styleSheet) {
+        // This is required for IE8 and below.
+        style.styleSheet.cssText = css;
+    } else {
+        style.appendChild(document.createTextNode(css));
     }
 })();
